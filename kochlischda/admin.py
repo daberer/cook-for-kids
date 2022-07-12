d188975b16b28d513d4e8d9c639dc3b82739b12c
daberer <daniel.aberer@geo.tuwien.ac.at>
2022-07-12 23:34:19 +0200

    Add Holiday table, fill kid table

--- kochlischda/admin.py
+++ kochlischda/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Kid, Dish
+from .models import Kid, Dish, Holiday
 
 admin.site.register(Kid)
 admin.site.register(Dish)
+admin.site.register(Holiday)
--- kochlischda/services.py
+++ kochlischda/services.py
@@ -9,18 +9,12 @@ def calculate_month():
     month = 1
     # printing the calendar
     print(calendar.month(year, month))
+    names = Kid.objects.all()
+    print(names)
     
 
 
 if __name__ == '__main__':
     calculate_month()
-    #kids_list = ['Antonia', 'Balthasar', 'Caroline', 'Dodo']
-    #menu_list = ['Algenbuger', 'Braten', 'Cornflakes', 'Dattelmuffins']
-    
-    a = Kid(name='Antonia', exempt=True, gender='girl', monthly_dishes_number=0)
-    b = Kid(name='Balthasar', exempt=False, gender='boy')
-
-    print(a)
-    print(b)
     
     
