2c40a550c3df3d5c45a6b7d96afec870fd0ed307
daberer <daniel.aberer@geo.tuwien.ac.at>
2022-07-29 16:27:30 +0200

    Style notday form

--- kochlischda/forms.py
+++ kochlischda/forms.py
@@ -36,7 +36,7 @@ class NotdaysForm(forms.Form):
     year = forms.ChoiceField(choices=years)
     month = forms.ChoiceField(choices=MONTHS, initial=str(month))
     kid = forms.ModelMultipleChoiceField(queryset=Kid.objects.all())
-    dates = forms.CharField(required=False)
-    dishes_this_month = forms.IntegerField(required=False)
-    wishdays = forms.BooleanField(required=False)
+    dates = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'e.g.: 1, 10-15'}))
+    dishes_this_month = forms.IntegerField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Number of meals to cook this month'}))
+    wishdays = forms.BooleanField(required=False, widget=forms.TextInput(attrs={'placeholder': 'check when dates are only possible dates'}))
     
