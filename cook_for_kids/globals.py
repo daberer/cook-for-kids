d70c4fd7e384b57a6bc7cc80091cb128096024ce
daberer <daniel.aberer@geo.tuwien.ac.at>
2025-05-23 11:00:17 +0200

    Bugfix globals

--- cook_for_kids/globals.py
+++ cook_for_kids/globals.py
@@ -1,5 +1,5 @@
-# class Setup:
-#     df = None
-#     optimise = False
-#     trial_number = 1000
-#     excursion_day = (True, 1)   #0 (Monday), 1 (Tuesday), 2 (Wednesday)..
+class Setup:
+    df = None
+    optimise = False
+    trial_number = 1000
+    excursion_day = (True, 1)   #0 (Monday), 1 (Tuesday), 2 (Wednesday)..
